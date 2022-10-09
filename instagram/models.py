7a53b141b07228e969963101a3bb69cd0b6e6e53
4books <joonwoo9192@gmail.com>
2022-10-09 19:15:41 +0900

    python

--- instagram/models.py
+++ instagram/models.py
@@ -2,6 +2,17 @@ from django.db import models
 
 
 class Post(models.Model):
-    message = models.TextField()
+    message = models.TextField(default="message")
+    photo = models.ImageField(blank=True, upload_to="instagram/post/%Y/%m/%d")
+    is_public = models.BooleanField(default=False, verbose_name="공개 여부")
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        # return f"Custom Post object({self.id})"
+        return self.message
+
+    # def message_length(self):
+    #     return len(self.message)
+
+    # message_length.short_description = "메세지 글자수"
